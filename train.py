073f668fd2699e51f16e9edc2308c58ced1994bf
RuslanPeresy <peresyolkov.ruslan@yandex.ru>
2024-05-16 14:41:15 +0800

    remove prints not needed

--- train.py
+++ train.py
@@ -8,8 +8,6 @@ def train_model(model, tokenizer, dataset, max_seq_length, train_args):
     gpu_stats = torch.cuda.get_device_properties(0)
     start_gpu_memory = round(torch.cuda.max_memory_reserved() / 1024 / 1024 / 1024, 3)
     max_memory = round(gpu_stats.total_memory / 1024 / 1024 / 1024, 3)
-    print(f"GPU = {gpu_stats.name}. Max memory = {max_memory} GB.")
-    print(f"{start_gpu_memory} GB of memory reserved.")
 
     trainer = SFTTrainer(
         model=model,
